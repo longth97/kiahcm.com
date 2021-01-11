b78095f5ebeac946c5e48e8b7a1fa5f3df1369b3
vansuphan <vansuphan88@gmail.com>
2021-01-12 01:17:15 +0700

    config postcss

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,10 +4,11 @@ import { AppProps } from "next/app";
 import React from "react";
 import { ApolloProvider } from "@apollo/client";
 import { client } from "../apollo/apollo";
+import Home from "./index";
 
 function App({ Component, pageProps }: AppProps) {
   return <ApolloProvider client={client}>
-    
+    <Component {...pageProps}/>
   </ApolloProvider>;
 }
 
