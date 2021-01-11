30249abfff75b23f6c82b0adcfafca05b5a3815e
LongTH <tranlong.kma@gmail.com>
2021-01-11 16:48:42 +0700

    fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,14 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
 
-import { AppProps } from 'next/app'
-import React from 'react'
-import { ApolloProvider } from '@apollo/client'
+import { AppProps } from "next/app";
+import React from "react";
+import { ApolloProvider } from "@apollo/client";
+import { client } from "../apollo/apollo";
 
 function App({ Component, pageProps }: AppProps) {
-  return (
-    <ApolloProvider client={client}>
-      
-    </ApolloProvider>
-  )
+  return <ApolloProvider client={client}>
+    
+  </ApolloProvider>;
 }
 
-export default App
+export default App;
