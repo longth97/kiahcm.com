370de0f794966a4358968e2836608c94988bee8b
vansuphan <vansuphan88@gmail.com>
2021-01-12 21:36:53 +0700

    .

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import MasterPage from "component/website/master/MasterPage";
-import Header from "component/website/elemets/Header";
+import Header from "component/website/elemets/HeaderCustom";
 import Footer from "component/website/elemets/Footer";
 
 export default function Home() {
