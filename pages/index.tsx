c176f8508e590447944ad20c150681ef0ef45fb4
LongTH <tranlong.kma@gmail.com>
2021-01-13 17:29:15 +0700

    doing

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,15 @@
-import Head from 'next/head'
+import Head from "next/head";
 import MasterPage from "component/website/master/MasterPage";
 import HeaderCustom from "component/website/elemets/HeaderCustom";
-import FooterCustom from "component/website/elemets/FooterCustom";
+import { CustomFooter } from "component/website/elemets/FooterCustom";
 
 export default function Home() {
   return (
-    <MasterPage pageName="Home" >
-        <HeaderCustom></HeaderCustom>
-        
-        <main id={"pHome"} className="homePage">
-          
-        </main>
-        <FooterCustom></FooterCustom>
+    <MasterPage pageName="Home">
+      <HeaderCustom />
+
+      <main id={"pHome"} className="homePage"></main>
+      <CustomFooter />
     </MasterPage>
-   
-  )
+  );
 }
