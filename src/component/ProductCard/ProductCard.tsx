e304c38d5bbf1ae7785c70367eeecf0b63c9ff4b
LongTH <tranlong.kma@gmail.com>
2021-01-15 15:17:30 +0700

    create model, common product card

--- src/component/ProductCard/ProductCard.tsx
+++ src/component/ProductCard/ProductCard.tsx
@@ -1,30 +1,28 @@
 import { CardMedia, Grid, Typography } from "@material-ui/core";
 import React from "react";
+import { Product } from "src/models/Product";
 import { DescriptionCar } from "./DescriptionCar";
 import { ImageCar } from "./ImageCar";
 import { CarName } from "./NameCar";
 import { PriceRectangle } from "./PriceRectangle";
 
-export const ProductCard = () => {
+export const ProductCard = (props?: Product) => {
   return (
     <div>
       <div className="wrapper">
         <div className="dashed-box">
-          <ImageCar
-            href="#"
-            src="https://mazda-automobile.vn/wp-content/uploads/2020/03/NEW-MAZDA2-SPORT-DO.png"
-          />
+          <ImageCar href={props.href ?? "#"} src={props.image} />
         </div>
         <div className="gold-box">
           <div className="name-title">
-            <CarName name="ALL NEW MAZDA3" href="#" />
+            <CarName name={props.name} href={props.href ?? "#"} />
           </div>
           <div className="name-description">
-            <DescriptionCar description=" NGOẠI THẤT MAZDA 3 Về kích thước tổng thể Mazda 3 2019, phiên bản SPORT LUXURY (Hatchback) và LUXURY (Sedan) chỉ khác nhau về chiều dài, các thông số khác gần như tương đồng nhau. Phiên bản hatchback" />
+            <DescriptionCar description={props.description} />
           </div>
         </div>
         <div className="green-box">
-          <PriceRectangle price={100000000} />
+          <PriceRectangle price={props.price} />
         </div>
       </div>
       <style jsx>
@@ -52,7 +50,7 @@ export const ProductCard = () => {
           }
           .green-box {
             position: absolute;
-            z-index: 3; 
+            z-index: 3;
             right: 1px;
             top: 60%;
           }
@@ -60,48 +58,4 @@ export const ProductCard = () => {
       </style>
     </div>
   );
-  // return (
-  //   <div>
-  //     <Grid
-  //       className="dashedBox"
-  //       container
-  //       direction="column"
-  //       alignItems="center"
-  //       style={{ maxWidth: "800px" }}
-  //     >
-  //       <Grid item style={{ maxHeight: "320px", minHeight: "220px" }}>
-  //         <ImageCar
-  //           href="#"
-  //           src="https://mazda-automobile.vn/wp-content/uploads/2020/03/NEW-MAZDA2-SPORT-DO.png"
-  //         />
-  //       </Grid>
-  //       <Grid className="price" item>
-  //         <PriceRectangle price={100000000} />
-  //       </Grid>
-  //       <Grid item>
-  //         <CarName name="ALL NEW MAZDA3" href="#" />
-  //       </Grid>
-  //       <Grid item>
-  //         <DescriptionCar description=" NGOẠI THẤT MAZDA 3 Về kích thước tổng thể Mazda 3 2019, phiên bản SPORT LUXURY (Hatchback) và LUXURY (Sedan) chỉ khác nhau về chiều dài, các thông số khác gần như tương đồng nhau. Phiên bản hatchback" />
-  //       </Grid>
-  //     </Grid>
-  //     <style jsx>
-  //       {`
-  //         .dashedBox {
-  //           position: relative;
-  //           z-index: 1;
-  //           border: dashed;
-  //           height: 8em;
-  //           margin-bottom: 1em;
-  //           margin-top: 2em;
-  //         }
-  //         .price {
-  //           z-index: 3;
-  //           position: absolute;
-  //           right: 0px;
-  //         }
-  //       `}
-  //     </style>
-  //   </div>
-  // );
 };
