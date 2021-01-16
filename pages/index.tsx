a63015491c12661f0fd6bf32fa908e2d4b3a3112
vansuphan <vansuphan88@gmail.com>
2021-01-16 11:29:38 +0700

    config globals

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,17 @@
-import Head from "next/head";
-import styles from "../styles/Home.module.css";
+
 import React from "react";
-import MenuList from "src/component/website/menu/MenuList";
-import Container from "src/component/website/elemets/Container";
-import { ProductCard } from "src/component/ProductCard/ProductCard";
+import MasterPage from "src/component/website/master/MasterPage";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Trang chủ - KIA Gò Vấp</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <header>
-        <Container>
-          <MenuList></MenuList>
-        </Container>
-      </header>
-      <main className={styles.main}></main>
 
-      <footer className={styles.footer}>
-        <a href="#">
-          <img src='kmasoft.png' height="50px" />
-          Design by KMASoft
-        </a>
-      </footer>
-    </div>
+    <MasterPage pageName="Trang chủ">
+
+      <main id="pHome" className="pHome">
+
+      </main>
+    
+    </MasterPage>
+
   );
 }
---
+++ src/constants.tsx
@@ -0,0 +1,11 @@
+import asset from "plugins/assets/asset";
+
+const ElementColor = {
+   
+    PRIMARY_COLOR : "#b90000", // red
+    SECONDARY_COLOR : "#424242", // black
+    THIRDARY_COLOR : "#384f65", // hover black
+   
+}
+
+export default ElementColor;
