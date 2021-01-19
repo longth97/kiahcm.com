94a3a443e57fc9c326e34e2babb83f4b906b57e5
vansuphan <vansuphan88@gmail.com>
2021-01-19 14:28:14 +0700

    add card-version-card

--- pages/test-su.tsx
+++ pages/test-su.tsx
@@ -4,7 +4,32 @@ import MenuList from "src/component/website/menu/MenuList";
 import Container from "src/component/website/elemets/Container";
 import CarouselProduct from "src/component/website/carousel/CarouselProduct";
 import MasterPage from "src/component/website/master/MasterPage";
-import DescriptionProduct from "src/component/website/description-product/DescriptionProduct"
+import DescriptionProduct from "src/component/website/description-product/DescriptionProduct";
+import TableProducts from "src/component/website/table-product/TableProduct";
+
+const fetData = [
+  {
+    name : "BT-50 Standard 4x4",
+    price : "590.000.000 ",
+    contacts : "0903 809 846",
+  },
+  {
+    name : "BT-50 Deluxe 4x2",
+    price : "625.000.000",
+    contacts : "0903 809 846",
+  },
+  {
+    name : "BT-50 Luxury 4x2",
+    price : "664.000.000",
+    contacts : "0903 809 846",
+  },
+  {
+    name : "BT-50 Premium 4x4",
+    price : "749.000.000",
+    contacts : "0903 809 846",
+  },
+
+]
 
 export default function Home() {
   const dateToFormat = new Date('1976-04-19T12:59-0500');
@@ -33,10 +58,13 @@ export default function Home() {
               ></DescriptionProduct>
 
             </div>
+            
 
           </div>
           
          
+          <TableProducts dataList={fetData}></TableProducts>
+
         </Container>
           
       </main>
