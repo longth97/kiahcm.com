bbcc3490c3cd52f3dbcdce3178c70c787f225e23
vansuphan <vansuphan88@gmail.com>
2021-01-20 22:17:17 +0700

    add effect home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,6 +11,9 @@ import PrimaryButton from "src/component/website/primary-button/PrimaryButton";
 import Title from "src/component/website/title/Title";
 import { Home } from "src/models/Home";
 import { HomeQuery } from "src/services/homeQuery";
+import SnowEffect from "src/component/website/effects/SnowEffect";
+import Container from "src/component/website/elemets/Container";
+
 
 export default function HomePage() {
   const { loading, error, data } = useQuery<Home>(HomeQuery)
@@ -20,35 +23,117 @@ export default function HomePage() {
     <MasterPage pageName="Trang chủ">
       <CustomCarousel />
       <main id="pHome" className="pHome">
+      <Container>
         <div className="body-products">
-          {
-            data.home.products.map((e, i) =>
-              <ProductCard
+            {
+              data.home.products.map((e, i) =>
+                <ProductCard
+                  key={i}
+                  id={e.id}
+                  name={e.name}
+                  price={e.price}
+                  image={e.image}
+                  description={e.description}
+                />
+              )
+            }
+          </div>
+          <div className="body-button">
+            <PrimaryButton text={"Xem tất cả các dòng xe"} />
+        </div>
+      </Container>
+        <div className="sectionBgEffect">
+          <div className="contentEffect">
+            <SnowEffect></SnowEffect>
+          </div>
+         
+          <Container>
+            <div className="listHowToBuy">
+              <div className="contentText copy">
+                <h1>Vì sao nên mua xe MAZDA???</h1>
+                <h2>TƯ VẤN NHIỆT TÌNH</h2>
+                <p>Đội ngũ nhân viên được đào tạo chuyên nghiệp, nhiều năm kinh nghiệm trong ngành luôn sẵn lòng giúp Quý khách tìm được chiếc xe ưng ý cũng như hỗ trợ Quý khách các thủ tục mua xe trả góp, đóng thuế, đăng ký, đăng kiểm xe…</p>
+                <h2>GIÁ TỐT NHẤT – GIAO XE NHANH</h2>
+                <p>Chúng tôi cam kết luôn mang lại mức giá tốt và nhiều ưu đãi nhất cho Quý khách cùng với thời gian giao xe sớm nhất tại khu vực phía Nam.</p>
+              </div>
+              <div className="contentText copy">
+                <h1>Vì sao nên mua xe MAZDA???</h1>
+                <h2>TƯ VẤN NHIỆT TÌNH</h2>
+                <p>Đội ngũ nhân viên được đào tạo chuyên nghiệp, nhiều năm kinh nghiệm trong ngành luôn sẵn lòng giúp Quý khách tìm được chiếc xe ưng ý cũng như hỗ trợ Quý khách các thủ tục mua xe trả góp, đóng thuế, đăng ký, đăng kiểm xe…</p>
+                <h2>GIÁ TỐT NHẤT – GIAO XE NHANH</h2>
+                <p>Chúng tôi cam kết luôn mang lại mức giá tốt và nhiều ưu đãi nhất cho Quý khách cùng với thời gian giao xe sớm nhất tại khu vực phía Nam.</p>
+              </div>
+            </div>
+            
+          </Container>
+          
+        </div>
+        <Container>
+          <Title title="Tin tức" />
+          <div className="body-news">
+            {
+              data.home.news.map((e, i) => <NewsCard
                 key={i}
-                id={e.id}
-                name={e.name}
-                price={e.price}
-                image={e.image}
+                date={e.createdAt}
+                url={e.image.url}
+                title={e.title}
                 description={e.description}
-              />
-            )
+              />)
+            }
+          </div>
+        </Container>
+        
+        <style jsx>{`
+        .contentEffect{
+          position: absolute;
+          width:100%;
+          height:100%;
+        }
+        .listHowToBuy{
+          display: flex;
+          justify-content: space-between;
+          z-index: 3;
+          padding: 80px 0;
+          &::after{
+            content: "";
+            position: absolute;
+            width: 0;
+            height: 0;
+            border-left: 30px solid transparent;
+            border-right: 30px solid transparent;
+            border-top: 30px solid #fff;
+            top: 0;
+            left: 50%;
+            transform: translate(-50%, 0);
           }
-        </div>
-        <div className="body-button">
-          <PrimaryButton text={"Xem tất cả các dòng xe"} />
-        </div>
-        <Title title="Tin tức" />
-        <div className="body-news">
-          {
-            data.home.news.map((e, i) => <NewsCard
-              key={i}
-              date={e.createdAt}
-              url={e.image.url}
-              title={e.title}
-              description={e.description}
-            />)
+          .contentText.copy{
+              position: relative;
+              width : 48%;
+              padding: 35px 50px;
+              z-index: 2;
+              color: #fff;
+              background-color: rgba(173, 173, 173,0.4);
+              letter-spacing: 2px;
+            h1{
+              padding : 10px 0;
+              padding-bottom: 15px;
+            }
+            h2{
+              padding : 10px 0;
+            }
           }
-        </div>
+        }
+          .sectionBgEffect{
+            justify-content: space-between;
+            position : relative;
+            margin-bottom: 40px;
+          }
+          .body-button{
+            width : 100%;
+            display: flex;
+            justify-content: center;
+          }
+        `}</style>
       </main>
     </MasterPage>
 
--- pages/test-su.tsx
+++ pages/test-su.tsx
@@ -78,6 +78,7 @@ export default function Home() {
       <style jsx>{`
         .contentProduct{
           display: flex;
+         
         }
         .contentDetails{
             width: 50%;
