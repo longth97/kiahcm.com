217c51ebe9a645854fbebd9dfc53949d1c711348
vansuphan <vansuphan88@gmail.com>
2021-01-21 21:48:15 +0700

    fix layput

--- pages/san-pham/detail.tsx
+++ pages/san-pham/detail.tsx
@@ -5,21 +5,54 @@ import CarouselProduct from "src/component/website/carousel/CarouselProduct";
 import DescriptionProduct from "src/component/website/description-product/DescriptionProduct";
 import { Product } from "src/models/Product";
 import { ProductDetailQuery } from "src/services/product.detail.query";
+import MasterPage from "src/component/website/master/MasterPage";
+import Container from "src/component/website/elemets/Container";
+// import TableProducts from "src/component/website/table-product/TableProduct";
 
 export default function DetailPage() {
     const { loading, error, data } = useQuery<Product>(ProductDetailQuery)
     if (loading) return <Loading />
     if (error) return <h1> Error: {error.message}</h1>
     return (
-        <div className="pProductDetail">
-            <div className="carousel-product-detail">
-                <CarouselProduct images={data.imagesCarousel} />
-                <DescriptionProduct
-                    nameProduct={data.name}
-                    price={data.price}
-                    codeProduct={data.name}
-                />
-            </div>
-        </div>
+        <MasterPage pageName="San pham">
+            <main id="pProduct" className="pProduct">
+                <Container>
+                    <div className="contentProduct">
+
+                        <div className="contentDetails carousel">
+                            <CarouselProduct images={data.imagesCarousel} />
+                        </div>
+
+                        <div className="contentDetails description">
+                        
+                        <DescriptionProduct
+                            nameProduct={data.name}
+                            price={data.price}
+                            codeProduct={data.name}
+                        />
+
+                        </div>
+                        
+                    </div>
+                    
+                </Container>
+
+                <style jsx>{`
+                    .contentProduct{
+                        display: flex;
+                    }
+                    .contentDetails{
+                        width: 50%;
+                        
+                    }
+                    .description{
+                        margin-left: 2%;
+                    }
+                `}</style>
+              
+            </main>
+            
+        </MasterPage>
+        
     )
 }
