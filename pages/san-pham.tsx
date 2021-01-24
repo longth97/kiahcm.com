0249a532244c07a301653eaabd56803c8a3d7663
Your Name <tranlong.kma@gmail.com>
2021-01-24 13:22:55 +0700

    add react hook form, modify layout

--- pages/san-pham.tsx
+++ pages/san-pham.tsx
@@ -5,6 +5,7 @@ import { Loading } from "src/component/Loading/Loading";
 import { NewsCard } from "src/component/NewsCard/NewsCard";
 import { ProductCard } from "src/component/ProductCard/ProductCard";
 import CustomCarousel from "src/component/website/carousel/Carousel";
+import Container from "src/component/website/elemets/Container";
 import MasterPage from "src/component/website/master/MasterPage";
 import Title from "src/component/website/title/Title";
 import { Products } from "src/models/Product";
@@ -18,88 +19,74 @@ export default function ProductPage() {
   return (
     <div>
       <MasterPage title="Sản phẩm">
-      <CustomCarousel />
-      <main id="pProduct" className="pProduct">
-        <div className="title-product">
-          <Title title="HATCHBACK" />
-        </div>
-        <div className="body-products">
-          {data.products
-            .filter((product) => product.segment === "HATCHBACK")
-            .map((e, i) => (
-              <ProductCard
-                key={i}
-                id={e.id}
-                name={e.name}
-                price={e.price}
-                image={e.image}
-                description={e.description}
-              />
-            ))}
-        </div>
-        <Title title="SEDAN" />
-        <div className="body-products">
-          {data.products
-            .filter((product) => product.segment === "SEDAN")
-            .map((e, i) => (
-              <ProductCard
-                key={i}
-                id={e.id}
-                name={e.name}
-                price={e.price}
-                image={e.image}
-                description={e.description}
-              />
-            ))}
-        </div>
-        <Title title="SUV" />
-        <div className="body-products">
-          {data.products
-            .filter((product) => product.segment === "SUV")
-            .map((e, i) => (
-              <ProductCard
-                key={i}
-                id={e.id}
-                name={e.name}
-                price={e.price}
-                image={e.image}
-                description={e.description}
-              />
-            ))}
-        </div>
+        <CustomCarousel />
+        <main id="pProduct" className="pProduct">
+          <Container>
+            <div className="title-product">
+              <Title title="HATCHBACK" />
+            </div>
+            <div className="body-products">
+              {data.products
+                .filter((product) => product.segment === "HATCHBACK")
+                .map((e, i) => (
+                  <ProductCard
+                    key={i}
+                    id={e.id}
+                    name={e.name}
+                    price={e.price}
+                    image={e.image}
+                    description={e.description}
+                  />
+                ))}
+            </div>
+            <Title title="SEDAN" />
+            <div className="body-products">
+              {data.products
+                .filter((product) => product.segment === "SEDAN")
+                .map((e, i) => (
+                  <ProductCard
+                    key={i}
+                    id={e.id}
+                    name={e.name}
+                    price={e.price}
+                    image={e.image}
+                    description={e.description}
+                  />
+                ))}
+            </div>
+            <Title title="SUV" />
+            <div className="body-products">
+              {data.products
+                .filter((product) => product.segment === "SUV")
+                .map((e, i) => (
+                  <ProductCard
+                    key={i}
+                    id={e.id}
+                    name={e.name}
+                    price={e.price}
+                    image={e.image}
+                    description={e.description}
+                  />
+                ))}
+            </div>
 
-        <Title title="MPV" />
-        <div className="body-products">
-          {data.products
-            .filter((product) => product.segment === "MPV")
-            .map((e, i) => (
-              <ProductCard
-                key={i}
-                id={e.id}
-                name={e.name}
-                price={e.price}
-                image={e.image}
-                description={e.description}
-              />
-            ))}
-        </div>
-        {/* 
-        <Title title="OTHER" />
-        <div className="body-products">
-          {
-            data.products.filter(product => product.segment != "MPV" && product.segment != "SUV" && product.segment != "SEDAN" && product.segment != "HATCHBACK").map((e, i) =>
-              <ProductCard
-                key={i}
-                id={e.id}
-                name={e.name}
-                price={e.price}
-                image={e.image}
-                description={e.description}
-              />
-            )
-          }
-        </div> */}
-      </main>
+            <Title title="MPV" />
+            <div className="body-products">
+              {data.products
+                .filter((product) => product.segment === "MPV")
+                .map((e, i) => (
+                  <ProductCard
+                    key={i}
+                    id={e.id}
+                    name={e.name}
+                    price={e.price}
+                    image={e.image}
+                    description={e.description}
+                  />
+                ))}
+            </div>
+          </Container>
+        </main>
       </MasterPage>
     </div>
   );
