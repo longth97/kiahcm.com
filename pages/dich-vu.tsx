55cd56f2c8e393b1e687058dd587f7839d471e93
Your Name <tranlong.kma@gmail.com>
2021-02-05 20:33:54 +0700

    đổi vị trí nút gọi

--- pages/dich-vu.tsx
+++ pages/dich-vu.tsx
@@ -18,25 +18,24 @@ export default function ProductPage() {
   if (data)
     return (
       <MasterPage title="Dịch vụ">
-          <main id="pService" className="pService">
-            <hr />
-            <Container>
-              {data.services.length === 1 ? (
-                <ReactMarkdown
-                  source={data?.services[0]?.content.markdown}
-                  escapeHtml={false}
-                  transformImageUri={(uri) =>
-                    uri.startsWith("http")
-                      ? uri
-                      : `${process.env.NEXT_PUBLIC_API_URL}${uri}`
-                  }
-                />
-              ) : (
-                <div>Chưa có dữ liệu</div>
-              )}
-            </Container>
+        <main id="pService" className="pService">
+          <hr />
+          <Container>
+            {data.services.length === 1 ? (
+              <ReactMarkdown
+                source={data?.services[0]?.content.markdown}
+                escapeHtml={false}
+                transformImageUri={(uri) =>
+                  uri.startsWith("http")
+                    ? uri
+                    : `${process.env.NEXT_PUBLIC_API_URL}${uri}`
+                }
+              />
+            ) : (
+              <div>Chưa có dữ liệu</div>
+            )}
+          </Container>
         </main>
-        
       </MasterPage>
     );
 }
