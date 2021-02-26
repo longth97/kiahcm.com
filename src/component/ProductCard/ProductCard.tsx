2471e8fd437dd5dd49247aeab7f60b85116252da
Long Trần (KMASoft) <tranlong.kma@gmail.com>
2021-02-26 20:14:25 +0700

    fix theo kh

--- src/component/ProductCard/ProductCard.tsx
+++ src/component/ProductCard/ProductCard.tsx
@@ -1,61 +1,62 @@
 import React from "react";
 import { Product } from "src/models/Product";
 import { PriceRectangle } from "./PriceRectangle";
-import { useRouter } from 'next/router'
+import { useRouter } from "next/router";
 
 export const ProductCard = (props?: Product) => {
   const router = useRouter();
   return (
     <div>
-      <div className="newsCard" onClick={()=>router.push(`/chi-tiet-san-pham/${props.id}`)}>
+      <div
+        className="newsCard"
+        onClick={() => router.push(`/chi-tiet-san-pham/${props.id}`)}
+      >
         <div className="box-image">
           <img src={props.image.url} />
         </div>
         <div className="content">
-          <h3 >
-            {props.name}
-          </h3>
-          <p>
-            {props.description}
-          </p>
-        </div>
-        <div className="price">
-          <PriceRectangle price={props.price} />
+          <h3>{props.name}</h3>
+          <div className="price">
+            <PriceRectangle price={props.price} />
+          </div>
+          <p>{props.description}</p>
         </div>
       </div>
       <style jsx>
-        {`.newsCard { 
+        {`
+          .newsCard {
             position: relative;
             .price {
-                position: absolute;
-                z-index: 2;
-                padding: 0px 30px 0px 30px;
-                top: 70%;
-                right: -50px;
-                text-align: center;
-                color: #FFF;
+              position: absolute;
+              z-index: 2;
+              padding: 0px 30px 0px 30px;
+              top: -50%;
+              right: -50px;
+              text-align: center;
+              color: #fff;
             }
             .box-image {
               img {
                 transition: 0.7s;
                 position: relative;
+                max-height: 300px;
                 z-index: 1;
                 transform: scale(1);
                 &:hover {
                   transform: scale(1.2);
                 }
+              }
             }
-            }
-           
-        .content {
-            padding-top: .7em;
-            position: relative;
-            width: 100%;
-            text-align: center;
-            background: white;
-            z-index: 2;
-            h3 { 
-                font-family: 'Roboto-Regular';
+
+            .content {
+              padding-top: 0.7em;
+              position: relative;
+              width: 100%;
+              text-align: center;
+              background: white;
+              z-index: 2;
+              h3 {
+                font-family: "Roboto-Regular";
                 font-size: 1.3em;
                 font-weight: 700;
                 text-overflow: ellipsis;
@@ -68,20 +69,19 @@ export const ProductCard = (props?: Product) => {
                 &:hover {
                   color: #b90000;
                 }
-            }
-            p {
-                font-family: 'Roboto-Thin';
-                font-size: 1.0em;
+              }
+              p {
+                font-family: "Roboto-Thin";
+                font-size: 1em;
                 text-overflow: ellipsis;
                 display: -webkit-box;
                 -webkit-line-clamp: 3;
                 -webkit-box-orient: vertical;
                 overflow: hidden;
                 text-align: left;
+              }
             }
-        }
-        
-    }
+          }
         `}
       </style>
     </div>
